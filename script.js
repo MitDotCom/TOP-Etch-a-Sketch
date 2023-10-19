dfde0c56519097a1454b0ad99d5819d44f45c8a4
Mitchell <Mitchellrcomrie@gmail.com>
2023-10-19 14:57:11 -0700

    Refactor grid to branch cells from rows outward or downward, rather than do a 'coordinate' type grid with two 'rows': 'rows' and 'columns' on separate axis'

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ easFrame = document.getElementById('eas-frame');
 easGrid = document.getElementById('eas-grid');
 
 const gridRows = document.createElement('div');
+gridRows.classList.add("gridRows");
 easGrid.appendChild(gridRows);
-const gridColumns = document.createElement('div');
-easGrid.appendChild(gridColumns);
 
-for (let i = 0; i < 20; i++) {
-    let `cellC${i}` = document.createElement('div');
-    gridColumns.appendChild(`cellC${i}`);
+for (let i = 0; i <= 16; i++) {
+    
+    let column = document.createElement('div');
+    column.classList.add(`${i}`);
+    gridRows.appendChild(column);
 };
 
-console.log(gridColumns);
