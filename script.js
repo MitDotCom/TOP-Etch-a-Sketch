d679f49776c84c450e81b954bc930f63f310f963
Mitchell <Mitchellrcomrie@gmail.com>
2023-10-19 13:35:55 -0700

    Change for loop spacing for readability

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ easGrid.appendChild(gridRows);
 const gridColumns = document.createElement('div');
 easGrid.appendChild(gridColumns);
 
-for (i=0;i<20;i++) {
+for (let i = 0; i < 20; i++) {
     let `cellC${i}` = document.createElement('div');
     gridColumns.appendChild(`cellC${i}`);
-}
+};
 
 console.log(gridColumns);
