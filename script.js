05de3c1187baf1ebc276030c598a8637c41a80b0
Mitchell <Mitchellrcomrie@gmail.com>
2023-10-19 15:46:07 -0700

    Add CSS to EaS frame and grid to create basic visual layout

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ for (let i = 0; i < 16; i++) {
 
         let cell = document.createElement('div');
         cell.classList.add(`cell${i}`);
-        console.log(cell);
         currentColumn.appendChild(cell);
     }
 
