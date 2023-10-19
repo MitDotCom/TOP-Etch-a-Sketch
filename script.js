d0ea9342229e298d4ed3f4996c161a4bad51cc87
Mitchell <Mitchellrcomrie@gmail.com>
2023-10-19 15:20:27 -0700

    GOT IT! I amade a 16x16 grid with: columns:0-15, which each have cells: 0-15

--- script.js
+++ script.js
@@ -6,10 +6,24 @@ const gridRows = document.createElement('div');
 gridRows.classList.add("gridRows");
 easGrid.appendChild(gridRows);
 
-for (let i = 0; i <= 16; i++) {
+// for loop to create gridRows
+for (let i = 0; i < 16; i++) {
     
     let column = document.createElement('div');
     column.classList.add(`column${i}`);
     gridRows.appendChild(column);
 };
 
+// for loop to create cells in each gridRows row
+for (let i = 0; i <= 16; i++) {
+
+    let currentColumn = document.querySelector(`.column${i}`);
+
+    for (let i = 0; i <= 16; i++) {
+
+        let cell = document.createElement('div');
+        cell.classList.add(`cell${i}`);
+        currentColumn.appendChild(cell);
+    }
+
+};
