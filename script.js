36dbe2dd9fefb178942ac5540ee8fdea9eab8c42
Mitchell <Mitchellrcomrie@gmail.com>
2023-10-23 14:50:25 -0700

    Add shake effect to frame

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-// Buttons
-const magic = document.querySelector('.magic');
-const screen = document.querySelector('.screen');
-const resetButton = document.getElementById('reset');
 
+
+// Variables
+const resetButton = document.getElementById('reset');
 // Etch-a-sketch grid
 const easFrame = document.getElementById('eas-frame');
 const easGrid = document.getElementById('eas-grid');
-const gridNodes = document.getElementsByClassName('cell');
-const gridSquares = Array.from(gridNodes);
-
 const gridRows = document.createElement('div');
+
+
 gridRows.classList.add("gridRows");
 easGrid.appendChild(gridRows);
 
 // for loop to create gridRows
-for (let i = 0; i < 22; i++) {
+for (let i = 0; i < 24; i++) {
     
     let column = document.createElement('div');
     column.classList.add(`column${i}`);
@@ -22,7 +20,7 @@ for (let i = 0; i < 22; i++) {
 };
 
 // for loop to create cells in each gridRows row
-for (let i = 0; i < 22; i++) {
+for (let i = 0; i < 24; i++) {
 
     let currentColumn = document.querySelector(`.column${i}`);
 
@@ -36,6 +34,9 @@ for (let i = 0; i < 22; i++) {
 
 };
 
+const gridNodes = document.getElementsByClassName('cell');
+const gridSquares = Array.from(gridNodes);
+
 gridSquares.forEach(square => {
     square.addEventListener('mouseover', (square) => {
         square.target.style.backgroundColor = 'black';
@@ -46,4 +47,9 @@ resetButton.addEventListener('click', () => {
     gridSquares.forEach(square => {
         square.style.backgroundColor = 'white';
     })
+    easFrame.classList.add('shake');
+    setTimeout(function () {
+        easFrame.classList.remove('shake');
+    }, 500)
+    
 })
